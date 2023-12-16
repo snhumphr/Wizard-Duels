1dca68ab432bbbab8e41ff16eb44b5405569f56f
snhumphr <humphreynsimon@gmail.com>
2023-12-15 17:41:16 -0700

    More work on multiplayer

--- menu.gd
+++ menu.gd
@@ -1,6 +1,8 @@
 extends MarginContainer
 
-const PORT : int = 1337
+const PORT = 7777
+const DEFAULT_SERVER_IP = "127.0.0.1" # IPv4 localhost
+const MAX_CONNECTIONS = 2
 
 func start_duel():
 	get_tree().change_scene_to_file("res://preduel.tscn")
@@ -10,12 +12,12 @@ func _on_quit_pressed():
 
 func _on_host_button_pressed():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT)
+	peer.create_server(PORT, MAX_CONNECTIONS)
 	multiplayer.multiplayer_peer = peer
 	start_duel()
 
 func _on_join_button_pressed():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client("localhost", PORT)
+	peer.create_client(DEFAULT_SERVER_IP, PORT)
 	multiplayer.multiplayer_peer = peer
 	start_duel()
