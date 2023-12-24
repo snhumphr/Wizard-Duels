d7ae98b2c99f47b77f3cb257fb2cf51ee2fe7cf6
Simon Humphrey <snhumphr@ualberta.ca>
2023-12-24 13:53:34 -0700

    Reworks the main menu, simplifying it from two scenes to one scene and allowing to choose the IP address of the host and the number of wizards for the server. Actually implementing 2+ wizard duels is still a work in progress. Also, this still needs to be tested on mac computers

--- menu.gd
+++ menu.gd
@@ -1,26 +1,53 @@
 extends MarginContainer
 
 const PORT = 7777
-const DEFAULT_SERVER_IP = "192.168.1.67" # IPv4 LAN
-const MAX_CONNECTIONS = 2
+var defaultIP
+var numButton
+var addressButton
+var numbers = [2, 3, 4, 5, 6, 7]
+var max_connections
+var numWizards = 1
 
 func _ready():
-	pass
+	numButton = self.get_node("MenuItems/HostBar/NumWizButton")
+	addressButton = self.get_node("MenuItems/JoinBar/IpAddressField")
+	for number in numbers:
+		numButton.add_item(str(number), number)
+	numButton.select(0) #Default to a 2 player duel
+	defaultIP = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
+	addressButton.set_text(defaultIP)
 
+func preduel():
+	self.get_node("MenuItems").set_visible(false)
+	self.get_node("WaitingLobby").set_visible(true)
+	multiplayer.peer_connected.connect(newWizard)
+	
+func newWizard(id: int):
+	print(multiplayer.is_server())
+	print(multiplayer.get_peers().size())
+	print(str(max_connections))
+	if multiplayer.is_server() and multiplayer.get_peers().size() + 1 == max_connections:
+		print("awoo")
+		self.rpc("start_duel")
+
+@rpc("authority", "reliable", "call_local")
 func start_duel():
-	get_tree().change_scene_to_file("res://preduel.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_host_button_pressed():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, MAX_CONNECTIONS)
+	max_connections = numButton.get_selected_id()
+	numWizards = max_connections
+	peer.create_server(PORT, max_connections)
 	multiplayer.multiplayer_peer = peer
-	start_duel()
+	preduel()
 
 func _on_join_button_pressed():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(DEFAULT_SERVER_IP, PORT)
+	defaultIP = addressButton.get_text()
+	peer.create_client(defaultIP, PORT)
 	multiplayer.multiplayer_peer = peer
-	start_duel()
+	preduel()
