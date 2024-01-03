24a73a50bb5e71c64cfeea143fa6872fa1cd19e3
snhumphr <humphreynsimon@gmail.com>
2024-01-03 16:19:35 -0700

    Adds descriptions to the offensive spells

--- SpellInfoTab.gd
+++ SpellInfoTab.gd
@@ -9,10 +9,8 @@ func init(spellArray):
 	for spell in spellArray:
 		if not spellDict.has(spell.name):
 			spellDict[spell.name] = spell.description
-			print(spell.description)
 		else:
 			assert(spellDict[spell.name] == spell.description)
 
 func displaySpellInfo(spell):
-	print("snork " + spellDict[spell])
 	self.set_text(spellDict[spell])
