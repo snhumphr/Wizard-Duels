2404ce49544c3f0764810d6c4b67fd974a6b46fd
snhumphr <humphreynsimon@gmail.com>
2024-02-06 12:41:45 -0700

    Adds more adjectives for monsters

--- monster.gd
+++ monster.gd
@@ -2,7 +2,7 @@ extends Entity
 class_name Monster
 
 @export var summoner_id: int = -1
-@export var adjectives: Array
+@export var adjectives: Array[String]
 
 @export var is_wizard: bool = false
 @export var is_monster: bool = true
